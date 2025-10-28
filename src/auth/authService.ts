a91f725c25ae493a5856b9f91520881001073afe
Wesley Lucas Cordeiro Fulgencio Taveira <wesley.fulgencio@AGU.GOV.BR>
2025-10-28 10:54:03 -0300

    fix(JWT): ajustes na autenticação JWT

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -1,50 +1,49 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { JwtService } from "@nestjs/jwt";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Usuario } from "src/infra/entities/Usuario";
-import { Repository } from "typeorm";
-import * as bcrypt from 'bcrypt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from 'src/infra/entities/Usuario';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private readonly jwtService: JwtService,
-        @InjectRepository(Usuario)
-        private readonly UserRepo: Repository<Usuario>
-    ) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(Usuario)
+    private readonly UserRepo: Repository<Usuario>,
+  ) {}
 
-    async validarUsuario(email: string, senha: string) {
-        console.log('🟢 Email recebido:', email);
-        const user = await this.UserRepo.findOne({where: {email}});
-        console.log('🟡 Usuário encontrado:', user);
-        
-        if (!user) return null;
+  async validarUsuario(email: string, senha: string) {
+    const user = await this.UserRepo.findOne({ where: { email } });
 
-        
-        console.log('🔵 Senha digitada:', senha);
-        console.log('🟣 Senha no banco:', user.senha);
+    if (!user) return null;
 
-        const senhaValida = await bcrypt.compare(senha, user.senha);
-        if (!senhaValida) return null;
+    const senhaValida = senha === user.senha;
 
-
-        const {senha: _, ...result } = user as any;
-        return result;
-        
+    if (!senhaValida) {
+      return null;
     }
 
-    async login(email: string, senha: string) {
-        const user = await this.UserRepo.findOne({where: {email}});
-        if (!user) throw new UnauthorizedException('Email ou senha inválidos.');
-        
-        const senhaValida = await bcrypt.compare(senha, user.senha);
-        if (!senhaValida) throw new UnauthorizedException('Email ou senha inválidos.');
+    const { senha: _, ...result } = user as any;
+    return result;
+  }
 
-        const payload = {sub: user.id, email: user.email };
-        const token = this.jwtService.sign(payload);
+  async login(email: string, senha: string) {
+    const user = await this.UserRepo.findOne({ where: { email } });
 
-        const {senha: _, ...userSafe } = user as any;
-        return {access_token: token, usuario: userSafe};
-        
+    if (!user) {
+      throw new UnauthorizedException('Email ou senha inválidos.');
     }
+    
+    const senhaValida = senha === user.senha;
+
+    if (!senhaValida) {
+      throw new UnauthorizedException('Email ou senha inválidos.');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    const { senha: _, ...userSafe } = user as any;
+    return { access_token: token, usuario: userSafe };
+  }
 }
